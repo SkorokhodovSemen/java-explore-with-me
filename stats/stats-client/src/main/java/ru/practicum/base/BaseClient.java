362195240324a998f26a1c0ae50f14d0687155e0
Semen <skorohodov-96@yandex.ru>
2024-03-26 18:42:30 +0300

    Fix client for checker

--- stats/stats-client/src/main/java/ru/practicum/base/BaseClient.java
+++ stats/stats-client/src/main/java/ru/practicum/base/BaseClient.java
@@ -1,12 +1,11 @@
 package ru.practicum.base;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.lang.Nullable;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Map;
 
 public class BaseClient {
@@ -18,7 +17,7 @@ public class BaseClient {
     }
 
     protected ResponseEntity<Object> get(String path, @Nullable Map<String, Object> parameters) {
-        return makeAndSendRequest(HttpMethod.GET, path, parameters, null);
+        return makeAndSendRequest(HttpMethod.GET, path, parameters, null, null);
     }
 
     protected <T> ResponseEntity<Object> post(String path, T body) {
@@ -26,7 +25,7 @@ public class BaseClient {
     }
 
     protected <T> ResponseEntity<Object> post(String path, @Nullable Map<String, Object> parameters, T body) {
-        return makeAndSendRequest(HttpMethod.POST, path, parameters, body);
+        return makeAndSendRequest(HttpMethod.POST, path, parameters, body, null);
     }
 
     private static ResponseEntity<Object> prepareClientResponse(ResponseEntity<Object> response) {
@@ -43,8 +42,9 @@ public class BaseClient {
     private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method,
                                                           String path,
                                                           @Nullable Map<String, Object> parameters,
-                                                          @Nullable T body) {
-        HttpEntity<T> requestEntity = new HttpEntity<>(body);
+                                                          @Nullable T body,
+                                                          Long userId) {
+        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders(userId));
         ResponseEntity<Object> statsServiceResponse;
         try {
             if (parameters != null) {
@@ -56,5 +56,14 @@ public class BaseClient {
             return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
         }
         return prepareClientResponse(statsServiceResponse);
+    }
+    private HttpHeaders defaultHeaders(Long userId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        if (userId != null) {
+            headers.set("X-Sharer-User-Id", String.valueOf(userId));
+        }
+        return headers;
     }
 }
